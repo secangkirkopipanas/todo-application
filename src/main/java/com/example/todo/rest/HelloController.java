7ef32a5f2e9204043c75bd256eb4b28b7adf5b83
Robertus Lilik Haryanto <rharyant@redhat.com>
2023-10-23 13:25:48 +0800

    Introduce HelloController

---
+++ src/main/java/com/example/todo/rest/HelloController.java
@@ -0,0 +1,16 @@
+package com.example.todo.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public Map<String, String> hello() {
+        return Map.of("message", "Hello World!");
+    }
+
+}
