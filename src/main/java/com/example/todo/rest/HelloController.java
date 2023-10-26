238ab29e5b28dae20826155204e03f5a77870453
Robertus Lilik Haryanto <rharyant@redhat.com>
2023-10-26 15:12:51 +0800

    Added new hello endpoint

--- src/main/java/com/example/todo/rest/HelloController.java
+++ src/main/java/com/example/todo/rest/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.todo.rest;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
@@ -11,6 +12,11 @@ public class HelloController {
     @GetMapping("/hello")
     public Map<String, String> hello() {
         return Map.of("message", "Hello World!");
+    }
+
+    @GetMapping("/hello/{name}")
+    public Map<String, String> hello(@PathVariable String name) {
+        return Map.of("message", "Hello, " + name + "!");
     }
 
 }
