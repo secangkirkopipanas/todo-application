300567b683554d77aed6edcedaadfa1d1895940f
robert <rharyant@redhat.com>
2024-06-04 22:13:34 +0800

    Fixed

--- src/main/java/com/example/todo/config/KafkaProducerConfig.java
+++ src/main/java/com/example/todo/config/KafkaProducerConfig.java
@@ -3,9 +3,9 @@ package com.example.todo.config;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
@@ -13,7 +13,7 @@ import org.springframework.kafka.core.ProducerFactory;
 import java.util.HashMap;
 import java.util.Map;
 
-@ConditionalOnProperty("spring.kafka.enabled")
+@Profile({"!test"})
 @Configuration
 public class KafkaProducerConfig {
 
--- src/test/java/com/example/todo/services/TodoItemServiceTest.java
+++ src/test/java/com/example/todo/services/TodoItemServiceTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,6 +16,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TodoItemServiceTest {
 
     @Autowired
