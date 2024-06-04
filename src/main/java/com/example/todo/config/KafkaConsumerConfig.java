82b4557e45ea05c9c412008cb59cd0b3ebbe0dab
robert <rharyant@redhat.com>
2024-06-04 17:11:51 +0800

    Added application properties for unit test

--- src/main/java/com/example/todo/config/KafkaConsumerConfig.java
+++ src/main/java/com/example/todo/config/KafkaConsumerConfig.java
@@ -3,6 +3,7 @@ package com.example.todo.config;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
@@ -13,6 +14,7 @@ import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import java.util.HashMap;
 import java.util.Map;
 
+@ConditionalOnProperty("spring.kafka.enabled")
 @EnableKafka
 @Configuration
 public class KafkaConsumerConfig {
--- src/main/java/com/example/todo/config/KafkaProducerConfig.java
+++ src/main/java/com/example/todo/config/KafkaProducerConfig.java
@@ -3,6 +3,7 @@ package com.example.todo.config;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
@@ -12,6 +13,7 @@ import org.springframework.kafka.core.ProducerFactory;
 import java.util.HashMap;
 import java.util.Map;
 
+@ConditionalOnProperty("spring.kafka.enabled")
 @Configuration
 public class KafkaProducerConfig {
 
