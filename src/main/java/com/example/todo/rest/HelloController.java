0519b95e0a32d3639e4e84c9ce5a4016ffc4023c
robert <rharyant@redhat.com>
2024-06-04 16:55:06 +0800

    Added Kafka

--- src/main/java/com/example/todo/rest/HelloController.java
+++ src/main/java/com/example/todo/rest/HelloController.java
@@ -1,5 +1,9 @@
 package com.example.todo.rest;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,21 +11,43 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 
 @RestController
+@Slf4j
 public class HelloController {
+
+    @Autowired
+    ObjectMapper jsonObjectMapper;
 
     @GetMapping("/hello")
     public Map<String, String> hello() {
-        return Map.of("message", "Hello World!");
+        Map<String, String> result = Map.of("message", "Hello World!");
+        try {
+            log.info("Return: ", jsonObjectMapper.writeValueAsString(result));
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+        return result;
     }
 
     @GetMapping("/hello/{name}")
     public Map<String, String> hello(@PathVariable String name) {
-        return Map.of("message", "Hello, " + name + "!");
+        Map<String, String> result = Map.of("message", "Hello, " + name + "!");
+        try {
+            log.info("Return: ", jsonObjectMapper.writeValueAsString(result));
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+        return result;
     }
 
     @GetMapping("/millis")
     public Map<String, Long> date() {
-        return Map.of("date", System.currentTimeMillis());
+        Map<String, Long> result = Map.of("date", System.currentTimeMillis());
+        try {
+            log.info("Return: ", jsonObjectMapper.writeValueAsString(result));
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+        return result;
     }
 
 }
--- src/main/java/com/example/todo/services/TodoItemService.java
+++ src/main/java/com/example/todo/services/TodoItemService.java
@@ -2,7 +2,10 @@ package com.example.todo.services;
 
 import com.example.todo.models.TodoItem;
 import com.example.todo.repositories.TodoItemRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
@@ -14,6 +17,12 @@ public class TodoItemService {
     @Autowired
     private TodoItemRepository todoItemRepository;
 
+    @Autowired
+    private KafkaTemplate<String, Object> kafkaTemplate;
+
+    @Autowired
+    private ObjectMapper jsonObjectMapper;
+
     public Optional<TodoItem> getById(Long id) {
         return todoItemRepository.findById(id);
     }
@@ -24,10 +33,18 @@ public class TodoItemService {
 
     public TodoItem save(TodoItem todoItem) {
         if (todoItem.getId() == null) {
-            todoItem.setCreatedAt(Instant.now());
+            todoItem.setCreatedAt(System.currentTimeMillis());
         }
-        todoItem.setUpdatedAt(Instant.now());
-        return todoItemRepository.save(todoItem);
+        todoItem.setUpdatedAt(System.currentTimeMillis());
+        todoItem = todoItemRepository.save(todoItem);
+
+        // Send the object into Kafka topic
+        try {
+            kafkaTemplate.send("todo", jsonObjectMapper.writeValueAsString(todoItem));
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+        return todoItem;
     }
 
     public void delete(TodoItem todoItem) {
