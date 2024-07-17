37f99192fa58e2c2bdbec044852cc42965e8d939
robert <rharyant@redhat.com>
2024-07-17 14:51:36 +0800

    Replace docker with podman

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ APP_IMAGE_TAG=latest
 PLATFORM=linux/amd64
 
 # Build container image
-docker build -t ${IMAGE_REGISTRY}/${IMAGE_PATH}/${APP_NAME}:${APP_IMAGE_TAG} --platform ${PLATFORM} -f src/container/Containerfile .
+podman build -t ${IMAGE_REGISTRY}/${IMAGE_PATH}/${APP_NAME}:${APP_IMAGE_TAG} --platform ${PLATFORM} -f src/container/Containerfile .
 
 # Push container image to registry
-docker push ${IMAGE_REGISTRY}/${IMAGE_PATH}/${APP_NAME}:${APP_IMAGE_TAG}
+podman push ${IMAGE_REGISTRY}/${IMAGE_PATH}/${APP_NAME}:${APP_IMAGE_TAG}
