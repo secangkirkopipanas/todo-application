40d50ef22cd4746bfb83308bb2a0df2dd52ce51e
robert <rharyant@redhat.com>
2025-02-28 17:32:38 +0800

    Fixed on issue for having custom context path

--- src/main/java/com/example/todo/TodoApplication.java
+++ src/main/java/com/example/todo/TodoApplication.java
@@ -2,9 +2,10 @@ package com.example.todo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TodoApplication {
+public class TodoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoApplication.class, args);
