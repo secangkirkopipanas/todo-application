4d103c60fad3ce4ba500d8163fd7d29dd6241e6b
robert <rharyant@redhat.com>
2025-03-03 16:27:37 +0800

    Upgraded versions

--- src/main/java/com/example/todo/TodoApplication.java
+++ src/main/java/com/example/todo/TodoApplication.java
@@ -2,9 +2,17 @@ package com.example.todo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.WebApplicationInitializer;
 
 @SpringBootApplication
-public class TodoApplication {
+public class TodoApplication extends SpringBootServletInitializer implements WebApplicationInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TodoApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoApplication.class, args);
