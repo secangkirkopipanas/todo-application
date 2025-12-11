938a1e738acf0a078a50945e1dfde7b6c2820765
Robertus Lilik Haryanto <robert.djokdja@gmail.com>
2025-12-11 13:52:47 +0800

    Fixed

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,20 +1,45 @@
 pipeline {
-    agent any
+    agent {
+        // Use the Kubernetes agent type
+        kubernetes {
+            defaultContainer 'maven'
+            inheritFrom 'maven-agent'
+            cloud 'openshift'
+            // Optional: Specify a service account if different from the default Jenkins one
+            // serviceAccount 'jenkins-agent'
+        }
+    }
+
     stages {
-        stage('Build') {
+
+        stage('Clone Repo') {
             steps {
-                sh '${MAVEN_HOME}/bin/mvn -B -DskipTests clean package'
+                // Manually specifies the repository URL and branch
+                git branch: 'main',
+                        credentialsId: 'github-token', // Optional: ID of a stored credential (e.g., SSH key)
+                        url: 'https://github.com/secangkirkopipanas/todo-application.git'
             }
         }
-//        stage('Test') {
-//            steps {
-//                sh '${MAVEN_HOME}/bin/mvn test'
-//            }
-//            post {
-//                always {
-//                    junit 'target/surefire-reports/*.xml'
-//                }
-//            }
-//        }
+
+        stage('Initialize and Checkout') {
+            steps {
+                script {
+                    def logFile = "build-output.log"
+
+                    echo "Running on dynamic Kubernetes agent with Maven..."
+                    sh "mvn --version"
+                    sh "mvn clean package >> ${logFile}"
+                    echo "Build output is stored in '${logFile}'"
+
+                    // 1. Archive the main application artifact (e.g., JAR/WAR)
+                    archiveArtifacts artifacts: 'target/*.jar',
+                            fingerprint: true, // Recommended: adds a cryptographic checksum for tracking usage
+                            allowEmptyArchive: false
+
+                    // Keep the log file as a build artifact on Jenkins
+                    archiveArtifacts artifacts: logFile
+                }
+            }
+        }
     }
 }
